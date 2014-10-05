6f52ca4b6b732cd60a97822b14fa34743f45853b
Otis <otis@hdtech.com.tw>
2014-10-05 14:24:31 +0800

    add output

--- crawler.rb
+++ crawler.rb
@@ -1,29 +1,54 @@
 require "Koala"
 require 'open-uri'
 require "CSV"
-ACCESS_TOKEN = "CAACEdEose0cBALQTuPVFPV1Bsuu0D4hreZBye9RhFriVQ0zerjVZCpqzyma2IyLHvV7ZAwJZBGa3BTZCMvxy4yupFLWwnUGNrgoBz5OvZAdJFYHi81qApQS5pzLzzLZC59jjCqoSZBh7yldpLbKDRwK8i8bYW3kHFK3pYPjr3Qgr8gLHJF7sJqsAQyrRKyhWWw8ASCbpZANh68riqLMBUkPdCCQsvz8aqI50ZD"
-@graph = Koala::Facebook::API.new(ACCESS_TOKEN)
+require File.join(File.dirname(__FILE__), 'config')
+
 CATEGORY_FILTER = ['politician' , 'public figure' , 'news/media website']
+FILE_NAME = "直轄市長"
+PATH = "data/#{FILE_NAME}.csv"
+OUTPUT_PATH = "data/#{FILE_NAME}_fb.csv"
+@graph = Koala::Facebook::API.new(ACCESS_TOKEN)
 
-def read(url)
-  CSV.new(open(url)).each do |row|
-    # use row here...
+def read(path)
+  unless File.file?(OUTPUT_PATH)
+    File.new(OUTPUT_PATH, "w+")
+  end
+  output_csv = CSV.open(OUTPUT_PATH, "wb")
+
+  CSV.foreach(path) do |row|
     area, user, party = row
-
+    fb_links = []
     puts "===============#{user}=================="
     puts '::PAGE::'
     result_pages =  @graph.search( user, type: 'page' )
     result_pages.select! do |page|
       category = page["category"].downcase
-      CATEGORY_FILTER.include? category
+      if CATEGORY_FILTER.include? category
+        fb_links << page.name
+        fb_links << "http://www.facebook.com/#{page.id}"
+      end
     end
     puts result_pages
+
     puts '::GROUP::'
     result_groups = @graph.search( user, type: 'group' )
+    result_groups.each do |group|
+      fb_links << group.name
+      fb_links << "http://www.facebook.com/#{group.id}"
+    end
+    puts result_groups
+    output_csv << (row + fb_links)
+  end
 
+end
+
+def output(area, user, party, fb_links)
+  CSV.open("path/to/file.csv", "wb") do |csv|
+  csv << ["row", "of", "CSV", "data"]
+  csv << ["another", "row"]
   end
 end
 
 
 
-read("https://raw.githubusercontent.com/kiang/elections/master/Console/Command/data/2014_candidates/%E9%84%89%E9%8E%AE%E5%B8%82%E9%95%B7.csv")
+read("data/直轄市長.csv")
