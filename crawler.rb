31de02f5d13a28c23a7a3fbea64906f8af14884a
Otis <otis@hdtech.com.tw>
2014-10-05 18:03:48 +0800

    add delay operation

--- crawler.rb
+++ crawler.rb
@@ -5,11 +5,11 @@ require File.join(File.dirname(__FILE__), 'config')
 
 CATEGORY_FILTER = ['politician' , 'public figure' , 'news/media website']
 FILE_NAME = "直轄市長"
-PATH = "data/#{FILE_NAME}.csv"
-OUTPUT_PATH = "data/#{FILE_NAME}_fb.csv"
+INPUT_PATH = "data/#{FILE_NAME}.csv"
+OUTPUT_PATH = "data/fb_#{FILE_NAME}.csv"
 @graph = Koala::Facebook::API.new(ACCESS_TOKEN)
 
-def read(path)
+def process(path)
 
   unless File.file?(OUTPUT_PATH)
     File.new(OUTPUT_PATH, "w+")
@@ -17,31 +17,54 @@ def read(path)
   output_csv = CSV.open(OUTPUT_PATH, "wb")
 
   CSV.foreach(path) do |row|
-    area, user, party = row
+    output = fetch_fb(*row)
+    puts "OUT: "
+    puts output
+    puts "...."
+    output_csv << output
+    delay()
+  end
+  output_csv.close()
+
+end
+
+def fetch_fb(area, user, party, mystery_number)
     fb_links = []
     puts "===============#{user}=================="
     puts '::PAGE::'
-    result_pages =  @graph.search( user, type: 'page' )
+    result_pages =  @graph.search( user, type: 'page' )[0,3]
     result_pages.select! do |page|
       category = page["category"].downcase
       if CATEGORY_FILTER.include? category
-        fb_links << page.name
-        fb_links << "http://www.facebook.com/#{page.id}"
+        fb_links << page["name"]
+        fb_links << "http://www.facebook.com/#{page["id"]}"
       end
     end
     puts result_pages
 
     puts '::GROUP::'
-    result_groups = @graph.search( user, type: 'group' )
+    result_groups = @graph.search( user, type: 'group' )[0,3]
     result_groups.each do |group|
-      fb_links << group.name
-      fb_links << "http://www.facebook.com/#{group.id}"
+      fb_links << group["name"]
+      fb_links << "http://www.facebook.com/#{group["id"]}"
     end
     puts result_groups
 
-    output_csv << (row + fb_links)
-  end
-
+    [area, user, party, fb_links]
 end
 
-read("data/直轄市長.csv")
+def delay
+  gate = 20
+  random = rand(100)
+
+  if random < gate
+    r = rand(10..25)
+    puts "...Delay...#{r}s"
+    sleep(r)
+  else
+    r = rand(1..5)
+    puts "...Delay...#{r}s"
+    sleep(r)
+  end
+end
+process(INPUT_PATH)
